c28d874ddc91ef31449b01543021260c8881e8c1
Ash Ramsey <ashramsey@gmail.com>
2011-06-04 12:45:12 +0800

    first commit

---
+++ Sandbox/SandboxUtilities.h
@@ -0,0 +1,16 @@
+//
+//  SandboxUtilities.h
+//  Sandbox
+//
+//  Created by Ashley Ramsey on 4/06/11.
+//  Copyright 2011 ashramsey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface SandboxUtilities : NSObject {
+    
+}
+
+@end
---
+++ Sandbox/SandboxUtilities.m
@@ -0,0 +1,14 @@
+//
+//  SandboxUtilities.m
+//  Sandbox
+//
+//  Created by Ashley Ramsey on 4/06/11.
+//  Copyright 2011 ashramsey. All rights reserved.
+//
+
+#import "SandboxUtilities.h"
+
+
+@implementation SandboxUtilities
+
+@end
